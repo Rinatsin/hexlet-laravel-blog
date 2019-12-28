b127e47087336c7002e7c652a6e076fef0c96b7d
rinat salimyanov <rinatsin@gmail.com>
2019-12-28 10:40:05 +0500

    Add page layout and directives

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,17 @@
 <?php ?>
+@extends('layouts.app')
 
-<h1>О блоге</h1>
+@section('title', 'O блоге')
+
+@section('header')
 <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
 
+@section('content')
 <p><b>Our team</b></p>
 @foreach ($team as $member)
     <p> Name: {{$member['name']}} </p>
     <p> Position: {{$member['position']}} </p>
     </br>
 @endforeach
+@endsection
